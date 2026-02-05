432954d910e90aff0b842160c5f42f988af73c7a
asifikbal09 <asif.ikbal662@gmail.com>
2026-02-05 17:34:40 +0600

    feat(user): add createAdminIntoDB function and handle profile photo upload

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs';
 import { prisma } from '../../shared/prisma';
 import { Request } from 'express';
 import { fileUploader } from '../../helper/fileUploader';
+import { UserRole } from '@prisma/client';
 
 const createPatientIntoDB = async (req: Request)=>{
 
@@ -9,7 +10,7 @@ const createPatientIntoDB = async (req: Request)=>{
       const uploadResult =  await fileUploader.uploadToCloudinary(req.file);
       req.body.patient.profilePhoto = uploadResult?.secure_url;
     }
-    console.log(req.body)
+    
 
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
@@ -28,7 +29,34 @@ const createPatientIntoDB = async (req: Request)=>{
     return result;
     
 }
+const createAdminIntoDB = async (req: Request)=>{
+
+    if(req.file){
+      const uploadResult =  await fileUploader.uploadToCloudinary(req.file);
+      req.body.admin.profilePhoto = uploadResult?.secure_url;
+    }
+    console.log(req.body)
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const result = await prisma.$transaction(async(tnx)=>{
+        await tnx.user.create({
+            data:{
+                email: req.body.admin.email,
+                password:hashedPassword,
+                role: UserRole.ADMIN
+            }
+        });
+
+        return tnx.admin.create({
+            data:req.body.admin
+        });
+    })
+    return result;
+    
+}
 
 export const UserService = {
-    createPatientIntoDB
+    createPatientIntoDB,
+    createAdminIntoDB
 }
